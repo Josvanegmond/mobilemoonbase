fb940f081bffe35829cf0ea57e998cfbeef63904
robikshrestha <robikshrestha@gmail.com>
2013-04-15 15:15:34 +0545

    Some classes we could use for the project (data models)

---
+++ Game docs/MoonVilleClasses.mm
@@ -0,0 +1,50 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1366015802241" ID="ID_437280646" MODIFIED="1366016749704" TEXT="MoonVille">
+<node CREATED="1366015927983" HGAP="-24" ID="ID_1708307407" MODIFIED="1366017666055" POSITION="right" TEXT="Resource" VSHIFT="-11">
+<attribute NAME="Launch mass" VALUE=""/>
+<attribute NAME="Price" VALUE=""/>
+<node CREATED="1366015935668" ID="ID_1944040806" MODIFIED="1366017056508" TEXT="Device">
+<node CREATED="1366015958595" ID="ID_1866237788" MODIFIED="1366015963109" TEXT="Power Source"/>
+<node CREATED="1366015965748" ID="ID_1055960755" MODIFIED="1366015968919" TEXT="Power Consumer">
+<node CREATED="1366016996628" ID="ID_1757129722" MODIFIED="1366017004593" TEXT="Vehicle"/>
+</node>
+</node>
+<node CREATED="1366015942149" ID="ID_1371671240" MODIFIED="1366015944851" TEXT="Material"/>
+</node>
+<node CREATED="1366015972980" HGAP="54" ID="ID_533213500" MODIFIED="1366017589057" POSITION="left" TEXT="Plant" VSHIFT="-23">
+<attribute_layout NAME_WIDTH="142" VALUE_WIDTH="110"/>
+<attribute NAME="Available Materials" VALUE=""/>
+<attribute NAME="Available Devices" VALUE=""/>
+<attribute NAME="Required Materials" VALUE="Name, quantity"/>
+<attribute NAME="Required Devices" VALUE="Name, quantity"/>
+<attribute NAME="OUTPUT" VALUE="device or material"/>
+</node>
+<node CREATED="1366016010182" HGAP="67" ID="ID_1899135721" MODIFIED="1366017661673" POSITION="right" TEXT="Power Grid" VSHIFT="-8">
+<attribute_layout VALUE_WIDTH="131"/>
+<attribute NAME="Sources" VALUE=""/>
+<attribute NAME="Consumers" VALUE="Plants"/>
+</node>
+<node CREATED="1366016755155" HGAP="43" ID="ID_1339252050" MODIFIED="1366017584497" POSITION="left" TEXT="Manager" VSHIFT="-14">
+<attribute_layout NAME_WIDTH="69" VALUE_WIDTH="183"/>
+<attribute NAME="Plants" VALUE="supply materials, devices, power"/>
+<attribute NAME="Materials" VALUE=""/>
+<attribute NAME="Devices" VALUE=""/>
+</node>
+<node CREATED="1366016947009" HGAP="80" ID="ID_1623790921" MODIFIED="1366017713323" POSITION="right" TEXT="Vehicle" VSHIFT="-11">
+<attribute_layout NAME_WIDTH="107"/>
+<attribute NAME="Power Source" VALUE=""/>
+<attribute NAME="From and To" VALUE=""/>
+<attribute NAME="Materials" VALUE=""/>
+</node>
+<node CREATED="1366017293202" HGAP="28" ID="ID_138153467" MODIFIED="1366017580717" POSITION="left" TEXT="Player" VSHIFT="-7">
+<attribute NAME="Budget" VALUE=""/>
+<attribute NAME="buy()" VALUE=""/>
+<attribute NAME="sell()" VALUE=""/>
+</node>
+<node CREATED="1366017599927" HGAP="31" ID="ID_60571134" MODIFIED="1366017741867" POSITION="right" TEXT="Other Corps" VSHIFT="-7">
+<attribute NAME="Material" VALUE=""/>
+<attribute NAME="Device" VALUE=""/>
+</node>
+</node>
+</map>
